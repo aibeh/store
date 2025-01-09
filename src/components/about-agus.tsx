2244f4d64e5933f05a088bf413312f4c6a3117bc
goncy <gonzalo.pozzo4@gmail.com>
2025-01-09 18:14:20 -0300

    Update text

--- src/components/about-agus.tsx
+++ src/components/about-agus.tsx
@@ -13,30 +13,32 @@ export function AboutAgus({store}: {store: Store}) {
           <div className="w-full flex-1 text-center md:text-left">
             <h2 className="mb-4 text-3xl font-light">¡Hola! Soy Agus :)</h2>
             <p className="mb-4 text-muted-foreground">
-              Estudiante de Lic. Nutrición (UBA). Mi misión es ayudarte a adoptar hábitos
-              alimenticios más saludables, brindándote opciones ricas, nutritivas, y sobre todo,
-              fáciles de incorporar en tu vida diaria a través de nuestras Viandas AIBEH.
+              Estudiante de Lic. Nutrición en la UBA, y mi misión es ayudarte a incorporar hábitos
+              alimenticios más saludables a tu vida. A través de nuestras Viandas AIBEH, quiero
+              ofrecerte opciones deliciosas, nutritivas y, sobre todo, fáciles de incluir en tu
+              rutina diaria.
             </p>
             <p className="mb-6 text-muted-foreground">
-              Aibeh, tiene el compromiso de ofrecer porciones variadas, reales y completas para
-              acompañarte en tu día a día. Mi objetivo no es solo ofrecerte un producto, sino
-              acompañarte en el camino de una alimentación más consciente, para que puedas sentirte
-              mejor, más energizado y saludable.
+              En AIBEH, nos comprometemos a brindarte porciones variadas, reales y completas para
+              acompañarte a lo largo de tu jornada. Mi objetivo es ser parte de tu proceso hacia una
+              alimentación más consciente, para que te sientas mejor, más energizado y saludable.
+            </p>
+            <p className="mb-6 text-muted-foreground">
+              Además, cuento con un excelente equipo que ayuda para que todo esto sea posible:
             </p>
             <ul className="mb-6 flex list-disc flex-col gap-4 text-muted-foreground">
               <li>
-                Bauti, mi novio y Prof. de Educación Física, se encarga de las redes, logística y
-                proveedores. Su dedicación asegura que todo funcione de manera fluida. Pero... No
-                pisa la cocina... jaja!
+                Bauti, mi novio y Prof. de Educación Física, se encarga de las redes sociales, la
+                logística y la gestión de proveedores, asegurando que todo funcione de manera
+                perfecta. Eso sí, ¡en la cocina no pisa! 😄
               </li>
               <li>
-                Eli, mi increíble ayudante de cocina, comparte mi pasión por la buena alimentación y
-                siempre está dispuesta a aportar su toque de creatividad y perfección en cada
-                receta.
+                Eli, mi increíble ayudante de cocina, comparte mi pasión por una alimentación
+                equilibrada y siempre aporta su toque de creatividad y perfección a cada receta.
               </li>
             </ul>
             <p className="text-muted-foreground">
-              Si llegaste hasta acá, ¡Gracias por confiar en Aibeh!
+              Si llegaste hasta acá, ¡Gracias por confiar en AIBEH!
             </p>
           </div>
         </div>
