53217a356644ea23de739bcf98bd6076f85c9a53
goncy <gonzalo.pozzo4@gmail.com>
2025-05-23 16:04:40 -0300

    Update color

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -38,7 +38,7 @@ const RootLayout = async ({children}: {children: React.ReactNode}) => {
               <HowItWorks />
             </header>
             <main className="flex flex-col gap-4 px-4">
-              <h2 className="flex flex-col text-center text-2xl font-medium md:text-4xl bg-gray-100 py-4">
+              <h2 className="flex flex-col text-center text-2xl font-medium md:text-4xl bg-foreground/50 py-4">
                 <span>¡Hacé tu pedido!</span>
               </h2>
               <CartProvider>{children}</CartProvider>
