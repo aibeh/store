d2fb6ffde94ed9bc816e391864c433807b673b35
aibeh <aibeh.tiendasaludable@gmail.com>
2025-11-21 13:07:44 -0300

    Update how-it-works.tsx

--- src/components/how-it-works.tsx
+++ src/components/how-it-works.tsx
@@ -6,16 +6,17 @@ export function HowItWorks() {
         <p className="text-muted-foreground">
           En AIBEH, ofrecemos un <span className="font-medium">Menú saludable</span> con{" "}
           <span className="font-medium">6 (seis)</span> opciones que se renuevan cada semana. Podes
-          elegir entre <span className="font-medium">Packs</span> de 5, 10 o 15 viandas, eligiendo las
+          elegir entre <span className="font-medium">Packs  de 5, 10 o 15 viandas</span> , eligiendo las
           opciones que más te gusten (¡y repetir sin problema!). Además, tenes la posibilidad de agrandar tu vianda a la hora de hacer el pedido.
         </p>
         <p className="mt-4 text-muted-foreground">
           Tomamos pedidos de <span className="font-medium">Viernes</span> a{" "}
           <span className="font-medium">Domingo</span>, y realizamos las{" "}
-          <span className="italic">entregas a domicilio</span>{" "}
-          <span className="font-medium">Miércoles</span> de{" "}
+          <span className="italic">entregas a domicilio los días</span>{" "}
+          <span className="font-medium">Miércoles</span> (Wilde, Don Bosco, Bernal y Quilmes) y{" "}
+          <span className="font-medium">Jueves</span> (Berazategui y Hudson) de{" "}
           <span className="font-medium">18:00 a 21:00 hs.</span> También, podés pasar a retirar tu
-          pedido por Bernal Centro / Wilde los días Jueves.
+          pedido por Bernal Centro / Wilde (Día y horario a coordinar).
         </p>
       </div>
     </section>
