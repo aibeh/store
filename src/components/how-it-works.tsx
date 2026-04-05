f9e871e50456e5dbe0ae10533b1233f746489119
aibeh <aibeh.tiendasaludable@gmail.com>
2026-04-05 10:36:45 -0300

    Update how-it-works.tsx

--- src/components/how-it-works.tsx
+++ src/components/how-it-works.tsx
@@ -15,8 +15,7 @@ export function HowItWorks() {
           <span className="italic">entregas a domicilio los días</span>{" "}
           <span className="font-medium">Miércoles</span> (Wilde, Don Bosco, Bernal y Quilmes) y{" "}
           <span className="font-medium">Jueves</span> (Berazategui y Hudson) de{" "}
-          <span className="font-medium">18:00 a 21:00 hs.</span> También, podés pasar a retirar tu
-          pedido por Bernal Centro / Wilde (Día y horario a coordinar).
+          <span className="font-medium">18:00 a 21:00 hs.</span> 
         </p>
       </div>
     </section>
